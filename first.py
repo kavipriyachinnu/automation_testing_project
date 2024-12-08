1f29a4722367c08dffae4ca77860ad0c638df319
kavipriyachinnu <kavipriyachinnu94@gmail.com>
2024-12-08 18:14:59 +0000

    updated with subtraction code

--- first.py
+++ first.py
@@ -1,5 +1,13 @@
 def add(a,b):
     return a+b
 
+def sub(c,d):
+    return c-d
+
 x = add(6,5)
-print(x) # print the files
+y = sub(7,8)
+
+print("Addition of Two Numbers:" ,x) # print the files
+print("Subtraction of two Numbers:", y)
+
+
